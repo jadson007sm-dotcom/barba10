da076298bc3141be31f123c0b852f5f504594799
jadson007sm-dotcom <jadson007sm@gmail.com>
2026-09-05 11:42:52 -0300

    feat: add Select UI component

---
+++ components/ui/select.tsx
@@ -0,0 +1,1 @@
+import {SelectHTMLAttributes} from 'react'; export function Select(p:SelectHTMLAttributes<HTMLSelectElement>){return <select className="w-full rounded-lg border border-gray-300 bg-white px-3 py-2" {...p}/>}
