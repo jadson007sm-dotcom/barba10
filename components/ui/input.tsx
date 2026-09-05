80353b629c030835a5b21f4bf572971c2c425337
jadson007sm-dotcom <jadson007sm@gmail.com>
2026-09-05 11:42:09 -0300

    feat: add Input UI component

---
+++ components/ui/input.tsx
@@ -0,0 +1,1 @@
+import {InputHTMLAttributes} from 'react'; export function Input(p:InputHTMLAttributes<HTMLInputElement>){return <input className="w-full rounded-lg border border-gray-300 bg-white px-3 py-2 outline-none focus:border-black" {...p}/>}
