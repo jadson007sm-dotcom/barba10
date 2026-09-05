67f263ff5dd839d21932bae488a85e98cd509748
jadson007sm-dotcom <jadson007sm@gmail.com>
2026-09-05 11:46:24 -0300

    feat: add barber dashboard

---
+++ app/barber/dashboard/page.tsx
@@ -0,0 +1,1 @@
+import {RolePage} from '@/components/RolePage'; export default function Page(){return <RolePage title="Barbeiro" links={['Minha agenda|/barber/appointments','Comissões|/barber/commissions']}/>}
